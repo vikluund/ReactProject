37630d12aeb086dd07b5c66cabc35e114393fa54
Tim <112405922+vikluund@users.noreply.github.com>
2023-04-13 18:03:46 +0200

    5

---
+++ react-page/src/components/Hero.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Hero() {
+  return (
+    <div>
+      <Wrap>
+        <HeroText>
+          <h1>Welcome</h1>
+          <h2>To My React</h2>
+        </HeroText>
+      </Wrap>
+    </div>
+  )
+}
+
+export default Hero
+
+const Wrap = styled.div`
+background: url('/media/big-data-g8e3476a48_1920.jpg');
+height: 100vh;
+display: flex;
+background-position: center;
+background-size: cover;
+position: relative;
+
+::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+`
+
+const HeroText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  z-index: 2;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+
+  h1 {
+  font-size: 12vh;
+  margin-bottom: 0;
+  }
+  h2 {
+  font-size: 6vh;
+  }
+`
